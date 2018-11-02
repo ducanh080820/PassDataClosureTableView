489d92c9986fc1febf9e66679e84831da8b20bbb
Trần Đức Anh <ducanhxom4@gmail.com>
2018-11-02 09:51:42 +0700

    Initial Commit

---
+++ PassDataClosureTableView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PassDataClosureTableView
+//
+//  Created by Trần Đức Anh on 11/2/18.
+//  Copyright © 2018 Trần Đức Anh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
